327cec89afcddcf87ae7aababcbfde44bfb5411f
simranjot singh <32786222+simranjots@users.noreply.github.com>
2021-05-31 17:28:07 -0400

    Added keyboard pod

--- CTC/AppDelegate.swift
+++ CTC/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreData
 import UserNotifications
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar =  false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside =  true
         //MARK: Firebase Configuration
         FirebaseApp.configure()
         
