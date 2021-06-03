9218433d391293ce98a7344ed1d0513bb1740167
simranjot singh <32786222+simranjots@users.noreply.github.com>
2021-06-02 22:59:53 -0400

    AddTodayViewController Update

--- CTC/ViewControllers/Home View/addTodayViewController.swift
+++ CTC/ViewControllers/Home View/addTodayViewController.swift
@@ -16,11 +16,7 @@ class addTodayViewController: UIViewController{
     var isOn : Bool = false
     
     ////variables
-    
-//    @IBOutlet weak var progressView: CircularProgressView!
-    
-    
-    
+ 
     @IBOutlet weak var progressView: CircularProgressView!
     
     
