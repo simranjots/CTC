1801a9412a0351175fdcd8c67aa99bc54ec44539
simranjot singh <32786222+simranjots@users.noreply.github.com>
2021-06-17 03:10:32 -0400

    profile changes

---
+++ CTC/ViewControllers/Home View/ReminderViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ReminderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+
+   
+
+}
--- CTC/ViewControllers/More Views/MorePageViewController.swift
+++ CTC/ViewControllers/More Views/MorePageViewController.swift
@@ -2,10 +2,82 @@ import UIKit
 
 class MorePageViewController: UIViewController {
     
-    //TableView outlet
+    //MARK: - Top View elements
+    let profileImageView: UIImageView = {
+       
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "Teresa Easler")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.tintColor = .white
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = UIColor.white.cgColor
+        return imageView
+    }()
+    
+    let editButton: UIButton = {
+        
+        let button = UIButton()
+        button.setImage(UIImage(named: "edit")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handleEditTap), for: .touchUpInside)
+        return button
+    }()
+    
+    let nameLabel: UILabel = {
+       
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "Arial Rounded MT Bold", size: 22)
+        label.text = "Teresa Easler"
+        return label
+    }()
+    
+    let emailLabel: UILabel = {
+       
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "Arial", size: 18)
+        label.text = "teresa.easler@gmail.com"
+        return label
+    }()
+    
+ 
+    
+    //MARK: - Properties of top view elements
+    
+    lazy var containerView: UIView = {
+        
+        let view = UIView()
+        
+        view.backgroundColor = Utilities.primaryTextColor
+        
+        view.addSubview(profileImageView)
+        profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileImageView.anchor(top: view.topAnchor, paddingTop: 90, width: 120, height: 120)
+        profileImageView.layer.cornerRadius = 120 / 2
+        
+        view.addSubview(editButton)
+        editButton.anchor(top: view.topAnchor, right: view.rightAnchor, paddingTop: 270, paddingRight: 20, width: 20, height: 20)
+        
+        view.addSubview(nameLabel)
+        nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameLabel.anchor(top: profileImageView.bottomAnchor, paddingTop: 10)
+        
+        view.addSubview(emailLabel)
+        emailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emailLabel.anchor(top:nameLabel.bottomAnchor, paddingTop: 5)
+        
+        return view
+    }()
+    
+    //MARK:- TableView outlet
     @IBOutlet var moreVCTableView: UITableView!
     
-    //Array of more options and icons
+    //MARK: - Array of more options and icons
+    
     let moreOptionsList = ["Practice History", "How to Use App", "About Program", "Frequently Asked Questions", "Join Facebook Community", "Privacy Policy", "Order Books", "About Authors", "Connect To Us", "Log Out"]
     
     let moreOptionIcons = ["practiceHistory", "howToUseApp", "aboutProgram", "FAQs", "joinFacebookCommunity", "privacyPolicy", "orderBooks", "aboutAuthors", "connectToUs", "logOut"]
@@ -13,15 +85,35 @@ class MorePageViewController: UIViewController {
     var currentUser: CurrentUser!
     var userObject: User?
     
+    //MARK: - LifeCycle Methods
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        super.viewDidLoad()
         
         moreVCTableView.delegate = self
         moreVCTableView.dataSource = self
         
         currentUser = CurrentUser()
         userObject = currentUser.checkLoggedIn()
+        
+        //Set containerView above tableview
+        view.addSubview(containerView)
+        containerView.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, height: 300)
+    
+        //Set containerView bottom constraints
+        moreVCTableView.anchor(top: containerView.bottomAnchor, paddingTop: 10)
+        
+    }
+   
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    //MARK:- Selector
+    
+    @objc func handleEditTap() {
+        performSegue(withIdentifier: Constants.Segues.moreToUpdateProfileSegue, sender: self)
     }
 }
 
@@ -116,3 +208,44 @@ extension MorePageViewController : UITableViewDelegate, UITableViewDataSource {
         }
     }
 }
+
+
+//MARK: - Extenstion for UIView
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, paddingTop: CGFloat? = 0, paddingLeft: CGFloat? = 0, paddingBottom: CGFloat? = 0, paddingRight: CGFloat? = 0, width: CGFloat? = nil, height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop!).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft!).isActive = true
+        }
+        
+        if let bottom = bottom {
+            if let paddingBottom = paddingBottom {
+                bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+            }
+        }
+        
+        if let right = right {
+            if let paddingRight = paddingRight {
+                rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+            }
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+                
+    }
+}
+
+
